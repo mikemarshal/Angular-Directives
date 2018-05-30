5c5d7752ccdb5bde7fdc6bbba33ef68319972f97
Michael Marshal <mikemarshalman@gmail.com>
2018-05-29 20:26:56 -0500

    solution to directives

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor () {
+    this.buttonPressed = false;
+    this.loggedPressCounter = 0;
+    this.loggedPress = [];
+  }
+
+  onPress() {
+    this.loggedPressCounter++;
+    this.loggedPress.push(this.loggedPress.length + 1);      
+    if (this.buttonPressed) {
+      this.buttonPressed = false;
+      return;
+    }
+    this.buttonPressed = true;
+  }
+}
